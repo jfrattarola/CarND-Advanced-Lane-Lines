a7eee89d301497a790a35b5f78062c1d4b7c75a3
Joshua Frattarola <jofratta@cisco.com>
2017-09-25 16:27:47 -0400

    latest

--- camera_cal.py
+++ camera_cal.py
@@ -12,8 +12,8 @@ def camera_cal_init(cal_images_path):
     image_points=[]
 
     #create object points for each intersection on the chessboard
-    objp = np.zeros((6*8,3), np.float32)
-    objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
+    objp = np.zeros((6*9,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
     #import all images
     images = glob.glob(os.path.join(cal_images_path, '*.jpg'))
@@ -25,14 +25,16 @@ def camera_cal_init(cal_images_path):
         
         #Convert image to grayscale
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        
+
         #find the corners
-        ret, corners = cv2.findChessboardCorners( gray, (8,6), None)
+        ret, corners = cv2.findChessboardCorners( gray, (9,6), None)
 
         #if corners are found, add them to the image and object points array
         if ret == True:
             image_points.append(corners)
             object_points.append(objp)
+        else:
+            print('ERROR reading {}'.format(fname))
 
     return object_points, image_points
 
@@ -41,11 +43,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str, default='camera_cal', 
                         help='directory to read calibration image files from')
+    parser.add_argument('--testdir', type=str, default='test_images', 
+                        help='directory to read calibration image files from')
     FLAGS, unparsed = parser.parse_known_args()
 
     object_points, image_points = camera_cal_init(FLAGS.dir)
 
-    images = glob.glob(os.path.join(FLAGS.dir, '*.jpg'))
+    images = glob.glob(os.path.join(FLAGS.testdir, '*.jpg'))
     for fname in images:
         image = mpimg.imread(fname)
         image_size = (image.shape[1], image.shape[0])
